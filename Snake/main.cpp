8e4a00b2bce689a5e339642ac854727d8a5ef710
andrewq16 <darthaq@gmail.com>
2019-01-09 11:18:43 -0500

    Initial Commit

---
+++ Snake/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  Snake
+//
+//  Created by Andrew Quinonez on 1/9/19.
+//  Copyright © 2019 Andrew Quinonez. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
